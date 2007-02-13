b50170ff8c7431c07d3be0cfb66011c79dff3210
Neil Lawrence <lawrennd@gmail.com>
2007-02-13 21:10:56 +0000

    Removed some older files from HGPLVM.

--- matlab/acclaimVisualise.m
+++
@@ -1,9 +0,0 @@
-function handle = acclaimVisualise(channels, skel, padding)
-%ACCLAIMVISUALISE For updating an Acclaim representation of 3-D data
-
-%MOCAP
-
-if nargin < 3 
-    padding = 0;
-end
-handle = skelVisualise(channels, skel, padding);
