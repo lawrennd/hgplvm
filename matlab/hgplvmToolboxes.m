cf0216a2f4a17b389acda02b47a1c2a88afa54b9
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2010-03-15 10:24:08 +0000

    Added hgplvm code.

---
+++ matlab/hgplvmToolboxes.m
@@ -0,0 +1,12 @@
+% HGPLVMTOOLBOXES Load in the relevant toolboxes for hgplvm.
+
+importLatest('netlab');
+importLatest('prior');
+importLatest('optimi');
+importLatest('datasets');
+importLatest('ndlutil');
+importLatest('mltools');
+importLatest('mocap');
+importLatest('kern');
+importLatest('gp');
+importLatest('fgplvm');
